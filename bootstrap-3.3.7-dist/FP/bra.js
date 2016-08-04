2e8405abea4039249ec40efa41298fcc7e696b1c
Semal <semal.ghaffar@gmail.com>
2016-08-04 10:07:38 -0500

    Bra Calc

--- bootstrap-3.3.7-dist/FP/bra.js
+++ bootstrap-3.3.7-dist/FP/bra.js
@@ -1,14 +1,35 @@
-function BaS(){
-	var BandInp = document.getElementById("BaM")
-	var BandS = 0
-	if (mod(BandInp%2)==0) {
+function BraS(){
+	var BandInp = Number(document.getElementById("BaM").value);
+	var BustInp = Number(document.getElementById("BuM").value);
+	var BandS = 0;
+	var CupS = 0;
+	var mo = BandInp%2;
+	if (mo==0) {
 		BandS = BandInp + 4;
 	} else { 
 		BandS = BandInp +5;	
 	}
-	BandS.innerHTML = str(BandS);
+
+	if ((BustInp-BandInp)==7){
+		CupS = "G";
+	} else if ((BustInp-BandInp)==6){
+		CupS = "F";
+	} else if ((BustInp-BandInp)==5) {
+		CupS = "DD";
+	} else if ((BustInp-BandInp)==4) {
+		CupS = "D";
+	} else if ((BustInp-BandInp)==3) {
+		CupS = "C";
+	} else if ((BustInp-BandInp)==2) {
+		CupS = "B";
+	} else if ((BustInp-BandInp)==1) {
+		CupS = "A";
+	} else if ((BustInp-BandInp)==0) {
+		CupS = "AA";
+	} else {
+		CupS= "?";
+	}
+	
+	document.getElementById("Answer").innerHTML = "" +BandS + CupS;
 }
 
-function BuS(){
-	var BustInp = document.getElementById("BuM")
-}
