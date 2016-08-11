f324e48f846070cd18b2075b3fe7ecb4555cb362
Madeline <etter.madeline@gmail.com>
2016-08-11 10:38:19 -0500

    final final

--- bootstrap-3.3.7-dist/FP/fit.js
+++ bootstrap-3.3.7-dist/FP/fit.js
@@ -6,47 +6,47 @@ function HMConverter() {
 			var wval = 0;
 			var hval = 0;
 // Bust
-			if (30 <= b && 31.5 >= b) {
-				bval = 0;
-			} else if (33 <= b && 34.5 >= b) {
-				bval = 1;
-			} else if (36 <= b && 37.5 >= b) {
+				if (30 <= b && 31.5 >= b) {
+					bval = 0;
+				} else if (31.5 < b && 34.5 >= b) {
+					bval = 1;
+				} else if (34.5 < b && 37.5 >= b) {
 					bval = 2;
-			} else if (39.5 <= b && 41 >= b) {
-				bval = 3;
-			} else if (45 <= b && 46 >= b) {
-				bval = 4;
-			} else {
-				bval = -1
-			}
+				} else if (37.5 < b && 41 >= b) {
+					bval = 3;
+				} else if (41 < b && 46 >= b) {
+					bval = 4;
+				} else{
+					bval = -1
+				}
 // Waist
 			if (23.5 <= w && 25.5 >= w) {
-				wval = 0;
-			} else if (26.5 <= w && 28.5 >= w) {
-				wval = 1;
-			} else if (30 <= w && 31.5 >= w) {
-				wval = 2;
-			} else if (33 <= w && 34.5 >= w) {
-				wval = 3;
-			} else if (37 <= w && 39.5 >= w) {
-				wval = 4;
-			} else {
-				wval = -1
-			}
+					wval = 0;
+				} else if (25.5 < w && 28.5 >= w) {
+					wval = 1;
+				} else if (28.5 < w && 31.5 >= w) {
+					wval = 2;
+				} else if (31.5 < w && 34.5 >= w) {
+					wval = 3;
+				} else if (34.5 < w && 39.5 >= w) {
+					wval = 4;
+				} else {
+					wval = -1
+				}
 // Hips
 			if (33 <= h && 34.5 >= h) {
-				hval = 0;
-			} else if (36 <= h && 38 >= h) {
-				hval = 1;
-			} else if (39.5 <= h && 41 >= h) {
-				hval = 2;
-			} else if (42.5 <= h && 44 >= h) {
-				hval = 3;
-			} else if (46 <= h && 48 >= h) {
-				hval = 4;
-			} else {
-				hval = -1
-			}
+					hval = 0;
+				} else if (34.5 < h && 38 >= h) {
+					hval = 1;
+				} else if (38 < h && 41 >= h) {
+					hval = 2;
+				} else if (41 < h && 44 >= h) {
+					hval = 3;
+				} else if (44 < h && 48 >= h) {
+					hval = 4;
+				} else {
+					hval = -1
+				}
 // Average
 			sum = bval + wval + hval
 			quotient = sum/3
--- bootstrap-3.3.7-dist/FP/hm.js
+++ bootstrap-3.3.7-dist/FP/hm.js
@@ -8,39 +8,39 @@ function HMCon() {
 				
 				if (30 <= b && 31.5 >= b) {
 					bval = 0;
-				} else if (33 <= b && 34.5 >= b) {
+				} else if (31.5 < b && 34.5 >= b) {
 					bval = 1;
-				} else if (36 <= b && 37.5 >= b) {
+				} else if (34.5 < b && 37.5 >= b) {
 					bval = 2;
-				} else if (39.5 <= b && 41 >= b) {
+				} else if (37.5 < b && 41 >= b) {
 					bval = 3;
-				} else if (45 <= b && 46 >= b) {
+				} else if (41 < b && 46 >= b) {
 					bval = 4;
 				} 
 				
 
 				if (23.5 <= w && 25.5 >= w) {
 					wval = 0;
-				} else if (26.5 <= w && 28.5 >= w) {
+				} else if (25.5 < w && 28.5 >= w) {
 					wval = 1;
-				} else if (30 <= w && 31.5 >= w) {
+				} else if (28.5 < w && 31.5 >= w) {
 					wval = 2;
-				} else if (33 <= w && 34.5 >= w) {
+				} else if (31.5 < w && 34.5 >= w) {
 					wval = 3;
-				} else if (37 <= w && 39.5 >= w) {
+				} else if (34.5 < w && 39.5 >= w) {
 					wval = 4;
 				} 
 				
 
 				if (33 <= h && 34.5 >= h) {
 					hval = 0;
-				} else if (36 <= h && 38 >= h) {
+				} else if (34.5 < h && 38 >= h) {
 					hval = 1;
-				} else if (39.5 <= h && 41 >= h) {
+				} else if (38 < h && 41 >= h) {
 					hval = 2;
-				} else if (42.5 <= h && 44 >= h) {
+				} else if (41 < h && 44 >= h) {
 					hval = 3;
-				} else if (46 <= h && 48 >= h) {
+				} else if (44 < h && 48 >= h) {
 					hval = 4;
 				} 
 
